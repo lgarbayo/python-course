d663d56b92a68ad5ad00d13855af0347e93b614a
lgarbayo <lgarbayo.lugarbayo@gmail.com>
2025-10-10 19:37:05 +0200

    feat: finished adding basics solutions

--- basics/02_basic.py
+++ basics/02_basic.py
@@ -1,3 +1,4 @@
+# Variables y asignación
 my_name = "lucho"
 print(my_name)
 
@@ -6,15 +7,15 @@ print(age)
 age=22
 print(age)
 
-#tipado dinamico: el tipo de dato se determina en tiempo de ejecucion
-#que no tienes que declararlo explicitamente
+# tipado dinamico: el tipo de dato se determina en tiempo de ejecucion, no tienes que declararlo explicitamente
 name="lucho"
 print(type(name))
 
 name=22
 print(type(name))
 
-#tipado fuerte: no realiza conversiones de tipo automaticas
+# tipado fuerte: no realiza conversiones de tipo automaticas 
+# Como vimos antes, no puedes hacer 2 + "hola". Debes convertir los tipos explícitamente.
 
 #f-string (literal de cadena de formato)
 print(f"Hola {my_name}, tengo {age -1 +1} años")
@@ -24,12 +25,12 @@ print(f"Hola {my_name}, tengo {age -1 +1} años")
 # Tienes 3 productos para comprar en un supermercado. Tu objetivo es imprimir: "huevos, bacon y morcilla"
 # Ejemplo: Huevos, Bacon, Morcilla
 
-mi_nombre_de_variable="lolo" #snake_case
+mi_nombre_de_variable="lolo" # en python para denominar variables se usan minúsculas separadas con guiones bajos: snake_case
 
-#NO TIENE CONSTANTES (UPPER_CASE)
+# PYTHON NO TIENE CONSTANTES (UPPER_CASE)
+# Por convención, si nombras una variable en mayúsculas, estás indicando a otros programadores que no deberían modificar su valor.
 
 # Palabras reservadas en Python (no se pueden usar como nombres de variables)
-
 # ['False', 'None', 'True', 'and', 'as', 'assert',
 # 'async', 'await', 'break', 'class', 'continue',
 # 'def', 'del', 'elif', 'else', 'except', 'finally',
@@ -37,9 +38,11 @@ mi_nombre_de_variable="lolo" #snake_case
 # 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
 # 'return', 'try', 'while', 'with', 'yield']
 
-#Anotaciones de tipo (opcional, para mayor claridad en el código)
-is_user_logged_in: bool = True #Indica que la variable es un booleano
+# Anotaciones de tipo (opcional, para mayor claridad en el código)
+is_user_logged_in: bool = True # Indica que la variable es un booleano: pista o anotación sobre el tipo de dato que deberia tener la variable
+# Python en sí mismo no fuerza que la variable sea de ese tipo, funciona solo como anotación
+# Sin embargo, herramientas externas de analisis de codigo pueden usar estas pistas para detecetar errores antes de que se ejecute el programa (p.ej. mypy)
 print(is_user_logged_in)
 
-#is_user_logged_in=42 CON TYPECHECK ACTIVADO DA ERROR
+#is_user_logged_in=42 CON TYPECHECK (mypy) ACTIVADO DA ERROR YA QUE DETECTA QUE NO ES BOOL
 #print(is_user_logged_in)
